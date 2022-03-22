e97d44165110f573fa3638b47385f26cf9fe114e
Ramiro Aisen <ramiroaisen@gmail.com>
2022-03-22 12:16:59 -0300

    add precompiled fiñes

---
+++ files/shims.js
@@ -0,0 +1,11 @@
+import './shims-53e326b8.js';
+import 'node:http';
+import 'node:https';
+import 'node:zlib';
+import 'node:stream';
+import 'node:buffer';
+import 'node:util';
+import 'node:url';
+import 'node:net';
+import 'node:fs';
+import 'node:path';
